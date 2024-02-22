0649d376d1e69a44e1765aa64ca3f4b67ee2fdec
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-02-22 15:32:46 +0100

    add function

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,8 +4,6 @@ let mail = document.getElementById("mail")
 
 let btn = document.getElementById("send")
 
-console.log(mail);
-
 let mailingList = ["123@gmail.com", "132@gmail.com", "213@gmail.com", "321@gmail.com"]
 
 console.log(mailingList);
@@ -13,5 +11,20 @@ console.log(mailingList);
 btn.addEventListener("click", myFunction)
 
 function myFunction() {
-    
+    if (mailingList.includes(document.getElementById("mail").value)) {
+        console.log("true");
+        
+    } else {
+        console.log("false");
+        
+    }
 }
+
+
+
+// function myFunction() {
+//     console.log(mail);
+//
+// }
+
+//() => console.log(document.getElementById("mail").value))
