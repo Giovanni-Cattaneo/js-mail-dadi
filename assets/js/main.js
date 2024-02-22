2722ca292f50381e8764c72e27b8e70b2aaab71b
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-02-22 15:06:15 +0100

    add array mailing list, add btn

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,13 @@
 console.log("hello world");
 
+let mail = document.getElementById("mail")
+
+let btn = document.getElementById("send")
+
+console.log(mail);
+
+let mailingList = ["123@gmail.com", "132@gmail.com", "213@gmail.com", "321@gmail.com"]
+
+console.log(mailingList);
+
+btn.addEventListener("click" myFunction)
