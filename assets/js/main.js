c335c7026e5a4243cdcd1250cc3936fdb66337b7
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-02-22 15:34:04 +0100

    add comment in js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,13 +4,13 @@ let mail = document.getElementById("mail")
 
 let btn = document.getElementById("send")
 
-let mailingList = ["123@gmail.com", "132@gmail.com", "213@gmail.com", "321@gmail.com"]
+let mailingList = ["123@gmail.com", "132@gmail.com", "213@gmail.com", "321@gmail.com"] // array della mailing list
 
 console.log(mailingList);
 
-btn.addEventListener("click", myFunction)
+btn.addEventListener("click", myFunction) // Al cliccare del btn atttiva la funzione
 
-function myFunction() {
+function myFunction() { // Funzione, al click verifica se il valore inserito nell'inpu mail è presente nella mailingList
     if (mailingList.includes(document.getElementById("mail").value)) {
         console.log("true");
         
@@ -20,11 +20,3 @@ function myFunction() {
     }
 }
 
-
-
-// function myFunction() {
-//     console.log(mail);
-//
-// }
-
-//() => console.log(document.getElementById("mail").value))
